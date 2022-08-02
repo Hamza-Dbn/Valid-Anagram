75c4908dde3ddb9c638bf31e3530478a3c07a3d2
Hamza Darbani <76852492+Hamza-Dbn@users.noreply.github.com>
2022-08-02 19:15:55 +0200

    Update code.js

--- code.js
+++ code.js
@@ -4,5 +4,12 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    
+    const sSorted = s.split("").sort().join();
+    const tSorted = t.split("").sort().join();
+    if (sSorted.length !== tSorted.length) return false;
+    for (i = 0; i < sSorted.length; i++) {
+        const theBase = sSorted[i]
+        if ( theBase !== tSorted[i] ) return false;
+    }
+    return true
 };
